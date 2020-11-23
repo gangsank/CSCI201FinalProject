b4f4b6e388d84c6e119a0e50c01023787bdad835
Gangsan Keum <gangsank@usc.edu>
2020-11-23 20:17:14 +0900

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,8 @@
 <?php
 
 define('DB_HOST', 'sql12.freesqldatabase.com');
-define('DB_USER', 'sql12376879');
-define('DB_PASS', 'xECNWmW2Lz');
-define('DB_NAME', 'sql12376879');
+define('DB_USER', 'sql12376692');
+define('DB_PASS', 'WNZtQAjnGY');
+define('DB_NAME', 'sql12376692');
+
+?>
